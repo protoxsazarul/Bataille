c116ebb6f8aa31723744b160b8f78e853cfc6f3b
Quentin <Quentincnudde6@gmail.com>
2019-07-02 11:15:33 +0200

    ajout de mappingCoord dans Grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,10 +1,9 @@
-import java.util.List;
-
 public class Grid {
     private Player player;
     private Spot[][] spots = new Spot[10][10];
     private boolean hasPlayed;
     private Boat[] boats = new Boat[5];
+    private java.util.Map<String, Integer> mappingCoord;
 
     public boolean isHasPlayed() {
         return hasPlayed;
@@ -20,11 +19,12 @@ public class Grid {
 
     /**
      * définit les spots sur lesquels un bateau est placé
+     *
      * @param boat le bateau à placer
      */
     private void boatPlacer(Boat boat) {
       /*
-      Regarder le nombre de spot à définir
+        Regarder le nombre de spot à définir
         Selection aléatoire d'une case de la grid
         selection aléatoire de l'orientation du bateau
         boucler si une des cases est deja prise ou si il n'y pas la place pour le boat
