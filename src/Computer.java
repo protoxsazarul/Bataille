bd7e2f11ebc0eab0bed97ad57a6aa961cfb4a12a
Quentin <Quentincnudde6@gmail.com>
2019-07-03 09:45:31 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/Computer.java
+++ src/Computer.java
@@ -1,4 +1,5 @@
 public class Computer extends Player {
     public void getChoice() {
+
     }
 }
--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,25 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Constants {
-    public final Scanner SCAN = new Scanner(System.in);
+    public static final Scanner SCAN = new Scanner(System.in);
+    public static final Map<Integer, String> MAPPING = new HashMap<>();
+    static  {
+        MAPPING.put(0,"a");
+        MAPPING.put(1,"b");
+        MAPPING.put(2,"c");
+        MAPPING.put(3,"d");
+        MAPPING.put(4,"e");
+        MAPPING.put(5,"f");
+        MAPPING.put(6,"g");
+        MAPPING.put(7,"h");
+        MAPPING.put(8,"i");
+        MAPPING.put(9,"j");
+
+    }
 }
